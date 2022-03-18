a4f0863ee56407b8fb0c9f0876e52317c5031f47
jujub444 <julio.l.almeidaa@gmail.com>
2022-03-18 16:49:38 -0300

    Exercício básico de contador usando linguagem javascript, página html, estilização css e o foco do exercício o contador usando js.

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let count = 0;
+
+const current_Number = document.getElementById('currentNumber');
+
+function increment() {
+    count++;
+    current_Number.innerHTML = count;
+}
+
+function decrement() {
+    count--;
+    current_Number.innerHTML = count;
+}
